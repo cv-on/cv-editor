a4d8b4c103acf5f4b77497a908474367963a792f
Thi Nguyen <anhthi.ieig@gmail.com>
2024-11-23 00:28:19 +0700

    Json stringify error

--- src/app/api/generate-pdf/route.ts
+++ src/app/api/generate-pdf/route.ts
@@ -52,6 +52,6 @@ export async function POST(request: Request) {
     });
   } catch (error: any) {
     console.log(error);
-    return NextResponse.json({ error }, { status: 500 });
+    return NextResponse.json({ error: JSON.stringify(error) }, { status: 500 });
   }
 }
